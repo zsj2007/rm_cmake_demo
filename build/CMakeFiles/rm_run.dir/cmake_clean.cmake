a03bb7373f4f090dafb960d298bfdca05dd61a78
zsj2007 <3039212048@qq.com>
2026-07-04 20:38:31 +0800

    初始提交：CMake测试工程

---
+++ build/CMakeFiles/rm_run.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/rm_run.dir/main.cpp.o"
+  "CMakeFiles/rm_run.dir/main.cpp.o.d"
+  "rm_run"
+  "rm_run.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/rm_run.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
